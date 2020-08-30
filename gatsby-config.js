666f6ae7b40107c5808d5482ba034b11a663b55e
prabhupant <prabhupant09@gmail.com>
2020-08-30 17:49:43 +0530

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Prabhu Pant`,
+    author: `Prabhu Pant`,
+    description: `Learning and Sharing`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/prabhupant09`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/prabhupant`,
+      },
+      {
+        name: `instagram`,
+        url: `https://www.instagram.com/prabhupant`,
+      },
+    ],
+  },
+}
